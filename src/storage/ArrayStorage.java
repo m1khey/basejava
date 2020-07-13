9d6282fc70673266231f568e9ae5ba6a511d757b
m1khey <ermo1ov@yandex.ru>
2020-07-13 23:37:53 +0400

    HW2 - my final version

--- src/storage/ArrayStorage.java
+++ src/storage/ArrayStorage.java
@@ -13,42 +13,47 @@ public class ArrayStorage {
 
     public void clear() {
         Arrays.fill(storage, null);
-
         size = 0;
     }
 
     public void save(Resume r) {
-        if (getIndex(r.getUuid()) == -1) {
-            storage[size] = r;
-
-            size++;
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume " + r.getUuid() + "already exist");
+        } else if (size==storage.length) {
+            System.out.println("Storage overflow");
         } else {
-            System.out.println("Resume " + r + " exist");
+            storage[size]=r;
+            size++;
         }
     }
 
     public void update(Resume r) {
-        if (getIndex(r.getUuid()) == -1) {
-            System.out.println("Resume " + r + " not exist ");
+        int index = getIndex(r.getUuid());
+        if (index != -1) {
+            System.out.println("Resume " + r.getUuid() + "not exist");
+        }  else {
+            storage[index]=r;
         }
     }
 
     public Resume get(String uuid) {
-        if (getIndex(uuid) == -1) {
+        int index = getIndex(uuid);
+        if (index == -1) {
             System.out.println("Resume " + uuid + " not exist ");
             return null;
         }
-        return storage[getIndex(uuid)];
+        return storage[index];
     }
 
     public void delete(String uuid) {
-        if (getIndex(uuid) == -1) {
+        int index = getIndex(uuid);
+        if (index == -1) {
             System.out.println("Resume " + uuid + " not exist ");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
         }
-
-        storage[getIndex(uuid)] = storage[size - 1];
-        storage[size - 1] = null;
-        size--;
     }
 
     /**
