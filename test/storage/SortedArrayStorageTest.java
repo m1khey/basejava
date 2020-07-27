f4681884f4d0f3763c34543a9f51bfada1e6e1dd
m1khey <ermo1ov@yandex.ru>
2020-07-28 01:20:20 +0400

    HW4

---
+++ test/storage/SortedArrayStorageTest.java
@@ -0,0 +1,10 @@
+package storage;
+
+import static org.junit.Assert.*;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
+    }
+}
