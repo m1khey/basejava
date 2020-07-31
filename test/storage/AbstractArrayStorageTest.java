c8e9dbd280334fae3a30d022897dc9b33f34abdf
m1khey <ermo1ov@yandex.ru>
2020-07-31 19:28:48 +0400

    changed test saveOverFlow

--- test/storage/AbstractArrayStorageTest.java
+++ test/storage/AbstractArrayStorageTest.java
@@ -1,6 +1,8 @@
 package storage;
 
-import exception.*;
+import exception.ExistStorageException;
+import exception.NotExistStorageException;
+import exception.StorageException;
 import model.Resume;
 import org.junit.Assert;
 import org.junit.Before;
@@ -83,7 +85,7 @@ public abstract class AbstractArrayStorageTest {
     @Test(expected = StorageException.class)
     public void saveOverflow() throws Exception {
         try {
-            for (int i = 4; i <AbstractArrayStorage.STORAGE_LIMIT ; i++) {
+            for (int i = 4; i <=AbstractArrayStorage.STORAGE_LIMIT ; i++) {
                 storage.save(new Resume());
             }
         } catch (StorageException e) {
