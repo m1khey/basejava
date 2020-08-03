13aedbd59a1c5f3ef905cd4540cc7fde3403c2d5
m1khey <ermo1ov@yandex.ru>
2020-08-03 21:59:01 +0400

    HW5

--- test/storage/SortedArrayStorageTest.java
+++ test/storage/SortedArrayStorageTest.java
@@ -1,14 +1,10 @@
 package storage;
 
-<<<<<<< HEAD
 import static org.junit.Assert.*;
 
-public class SortedArrayStorageTest extends AbstractArrayStorageTest {
-=======
-public class SortedArrayStorageTest extends AbstractStorageTest {
->>>>>>> HW5
+public class SortedArrayStorageTest extends AbstractStorageTest{
 
-    public SortedArrayStorageTest() {
+    public SortedArrayStorageTest(){
         super(new SortedArrayStorage());
     }
 }
