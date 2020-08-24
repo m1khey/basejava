ea8116cdf7b1c2f69a29fc75248ae91f67b09356
m1khey <ermo1ov@yandex.ru>
2020-08-25 00:35:15 +0400

    Lesson10 json

--- test/storage/AllStorageTest.java
+++ test/storage/AllStorageTest.java
@@ -9,7 +9,10 @@ import org.junit.runners.Suite;
         SortedArrayStorageTest.class,
         ListStorageTest.class,
         MapUuidStorageTest.class,
-        MapResumeStorageTest.class
+        MapResumeStorageTest.class,
+        ObjectPathStorageTest.class,
+        XmlPathStorageTest.class,
+        JsonPathStorageTest.class
 })
 public class AllStorageTest {
 }
