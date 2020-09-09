e8f85ad51d46a8b72229e8a9e3d5dd5ad031855d
m1khey <ermo1ov@yandex.ru>
2020-09-09 22:09:51 +0400

    Lesson14 HW13

---
+++ test/storage/SqlStorageTest.java
@@ -0,0 +1,10 @@
+package storage;
+
+import util.Config;
+
+public class SqlStorageTest extends AbstractStorageTest{
+
+    public SqlStorageTest(){
+        super(Config.get().getStorage());
+    }
+}
