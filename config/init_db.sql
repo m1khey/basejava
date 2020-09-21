cce21e43fe4a5df06b0ec62c67c0f0e65b69a61e
m1khey <ermo1ov@yandex.ru>
2020-09-21 22:46:45 +0400

    Lesson16 HW15_DB

--- config/init_db.sql
+++ config/init_db.sql
@@ -11,3 +11,14 @@ CREATE TABLE contact (
 );
 CREATE UNIQUE INDEX contact_uuid_type_index
     ON contact (resume_uuid, type);
+
+CREATE TABLE section (
+    id serial primary key,
+    resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+    type TEXT NOT NULL ,
+    content TEXT NOT NULL
+);
+CREATE UNIQUE INDEX section_idx
+    ON section (resume_uuid, type);
+
+
